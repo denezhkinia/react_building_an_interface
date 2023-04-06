3e45914c1c3b60bad844254cdd6beff8b7203894
Ilya Denezhkin <denezhkinia@gmail.com>
2023-04-06 23:43:56 +0400

    feat(set up and install) - add react App typescript - add react-icons - add tailwind

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { FaReacteurope } from "react-icons/fa";
+
+function App() {
+  return (
+    <div className="container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">
+        <FaReacteurope className="inline-block text-blue-400 align-top" />{" "}
+        React.js: Build an interface
+      </h1>
+    </div>
+  );
+}
+
+export default App;
